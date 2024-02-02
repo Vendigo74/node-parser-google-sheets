c3c38b283ffc29371caae4b4b2ebfaf1f6710398
Vendigo74 <tumypjkee225500@gmail.com>
2024-02-02 10:54:38 +0500

    nestjs init

---
+++ src/sheets/sheets.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { SheetsService } from './sheets.service';
+
+@Controller('sheets')
+export class SheetsController {
+    constructor(private sheetsService: SheetsService) {}
+
+    @Get()
+    async getJSON() {
+        const data = await this.sheetsService.getJSON();
+        console.log('data in controller is', data);
+        return data;
+    }
+}
